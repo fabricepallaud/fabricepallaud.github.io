2beca23323b01e0d4131aa463be4a95b9eb46d3a
Fabrice Pallaud <fabpallaud@gmail.com>
2021-03-20 17:45:34 +0000

    commit

---
+++ cc4a309.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{264:function(n,e,t){"use strict";t.r(e);var c={props:{config:{type:Object,default:null}}},l=t(23),component=Object(l.a)(c,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("section",{staticClass:"container"},[n._v("\n  Large banner\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
