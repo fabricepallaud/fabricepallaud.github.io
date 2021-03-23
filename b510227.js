6d15b44f3d26bdc60554244a7b5fc2367f501db8
Fabrice Pallaud <fabpallaud@gmail.com>
2021-03-23 11:55:39 +0000

    commit

--- b510227.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{271:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push("/omni")}},r=e(23),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
